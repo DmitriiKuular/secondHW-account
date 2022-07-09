a94eb55c7a7296096698bb435ee7d0f0b0272c05
Дмитрий Куулар <kuulardmo@bk.ru>
2022-07-09 13:34:27 +0700

    Added coge of tusk

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int startingBill = 100; // Начальный счет клиента
+        int amountPlus = 1359; // Сумма пополнения
+        int amountRubles = 100; // Количество рублей, за которые начсляется 1 рубль
+        int amountBonus;
+
+        if (amountPlus > 1000) {
+            amountBonus = amountPlus / amountRubles;
+        } else {
+            amountBonus = 0;
+        }
+        int finalBill = startingBill + amountPlus + amountBonus;
+
+        System.out.println("Итоговый счет - " + finalBill);
+        System.out.println("Количество бонусных рублей - " + amountBonus);
+    }
+}
